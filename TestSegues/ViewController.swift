515e2df11246635841eac0c9df13148fe0348824
Rudi Farkas <rudi.farkas@gmail.com>
2020-04-28 18:02:08 +0200

    Initial Commit

---
+++ TestSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSegues
+//
+//  Created by Rudolf Farkas on 28.04.20.
+//  Copyright © 2020 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
